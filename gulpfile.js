6fb2614eb93a3d4b917d2271149a5c933f2d448a
Simon Babay <simon@babay.fr>
2016-04-20 00:23:24 +0200

    Update gulp tasks + Sourcemaps inline

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,52 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var sourcemaps = require('gulp-sourcemaps');
+var cache = require('gulp-cached');
 
-var distDir = 'lib/';
-var srcFiles = 'src/**/*.es6';
+var paths = {
+    dest: 'lib/',
+    sources: 'src/**/*.es6',
+    assets: 'src/**/!(*.es6)',
+};
 
 /**
  * Default task
  */
-gulp.task('default', ['babel']);
+gulp.task('default', ['watch']);
 
 /**
- * Babel transpile source
+ * Build all
  */
-gulp.task('babel', function(){
-    return gulp.src(srcFiles)
-        .pipe(sourcemaps.init())
-        .pipe(babel())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(distDir));
+gulp.task('build', ['babel', 'assets']);
+
+/**
+ * Copy assets
+ */
+gulp.task('assets', function(){
+    return gulp.src(paths.assets)
+        .pipe(cache('assets-dev'))
+        .pipe(gulp.dest(paths.dest));
 });
 
-gulp.task('babel:watch', function(){
-    return gulp.watch(srcFiles, ['babel']);
+/**
+ * Babel transpile all sources
+ */
+gulp.task('babel', function(){
+    return gulp.src(paths.sources)
+        .pipe(cache('babel-dev'))
+        .pipe(sourcemaps.init())
+        .pipe(babel())
+        .on('error', function(err){
+            console.error(err.stack);
+            this.emit('end');
+        })
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.dest));
+});
+
+/**
+ * Babel watch
+ */
+gulp.task('watch', function(){
+    return gulp.watch([paths.sources, paths.assets], ['babel', 'assets']);
 });
