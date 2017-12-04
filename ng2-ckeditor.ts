c44e44127b6ef5ad67ec7b9d7d7158cb6e7f4053
Simon Babay <simon@babay.fr>
2017-12-04 20:45:34 +0100

    Fix ngc build

---
+++ ng2-ckeditor.ts
@@ -0,0 +1,4 @@
+export { CKEditorModule } from './src/ckeditor.module';
+export { CKEditorComponent } from './src/ckeditor.component';
+export { CKButtonDirective } from './src/ckbutton.directive';
+export { CKGroupDirective } from './src/ckgroup.directive';
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { CKEditorModule } from './ckeditor.module';
-export { CKEditorComponent } from './ckeditor.component';
-export { CKButtonDirective } from './ckbutton.directive';
-export { CKGroupDirective } from './ckgroup.directive';
