9ddf14195c0475692c8b06c1ed3f4bd246f3abc3
Krzysztof Zimny <zimny@znf.ch>
2021-12-13 13:54:15 +0100

    Update to angular v13

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { CKEditorComponent } from './ckeditor.component';
 export { CKEditorModule } from './ckeditor.module';
+export { CKButtonDirective } from './ckbutton.directive';
+export { CKGroupDirective } from './ckgroup.directive';
